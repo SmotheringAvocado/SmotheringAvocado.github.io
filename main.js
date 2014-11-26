230b27ef20266d2cc6c3398c01440b9ac33b1e71
EnergySquid <callender.w@gmail.com>
2014-11-26 10:07:37 -0500

    Test to see if I can find what was wrong

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-$(document).ready(function() {
-	$('.navbar').hover(
-		function() {
-			$(this).addClass('navbarActive');
-		},
-		function() {
-			$(this).removeClass('navbarActive');
-		}
-	);
-	$('#title'.hover(
-		function() {
-			$(this).addClass('titleActive');
-		},
-		function() {
-			$(this).removeClass('titleActive');
-		}
-	);
-})
+$(document).ready(function(){
+
+  $('div').hover(
+    function(){
+        $(this).addClass('navbarActive');
+    },
+    function(){
+        $(this).removeClass('navbarActive');
+    }
+  );
+
+});
