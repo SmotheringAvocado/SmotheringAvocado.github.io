0b967197762f245626be4f4938e071bc11b4e3ed
EnergySquid <callender.w@gmail.com>
2014-11-28 18:24:41 -0500

    Minor changes to the code

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
 	$('.navbar').hover(
 		function() {
-				$(this).addClass('navbarActive');
 			$(this).addClass('navbarActive');
 		},
 		function() {
-				$(this).removeClass('navbarActive');
 			$(this).removeClass('navbarActive');
 		}
 	);
